84f0d2d33337d6cb66d4833fbb8e607d9149fee6
jamalainm <robot@inman.xyz>
2022-02-04 22:18:20 -0600

    That's all for tonight; Just over 100 to sweep for bad Greek at the moment.

--- clean_text.py
+++ clean_text.py
@@ -109,7 +109,7 @@ def find_greek_words():
 #            elif bool(re.search(r'(Ð)', text)):
 #                greek.append(fn)
 # Need to move '|' to another search. It seems preoccupied with numbers
-            elif bool(re.search(r'[¶&‰¾=«»±§Ø°¤Æãç¢ÐéêëËÈÊÍÎjJþóòðÒÜÙÚŲwŸž]', text)):
+            elif bool(re.search(r'[¶&‰¾=«»±§Ø°¤Æãç¢ÐéêëËÈÊÍÎjJþóòðÒÜÙÚŲwŸž]', text)):
                 greek.append(fn)
 
     for gk in greek:
